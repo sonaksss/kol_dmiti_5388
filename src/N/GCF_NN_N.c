ecea8237f335985da5ca7e5d65d59b4c560305e8
eremchenko <nevfaded@gmail.com>
2026-04-30 13:54:32 +0300

    GCF_NN_N, NZER_N_B done

---
+++ src/N/GCF_NN_N.c
@@ -0,0 +1,30 @@
+#include "GCF_NN_N.h"
+#include "NZER_N_B.h"
+#include "MOD_NN_N.h"
+#include "COM_NN_D.h"
+
+/*
+Автор: Еремченко Пётр
+Группа: 5388
+*/
+
+/*
+НОД натуральных чисел
+
+Принимает 2 натуральных числа
+Вычисляет НОД и возвращет натуральное число
+*/
+NUMBN* GCF_NN_N(NUMBN* a, NUMBN* b){
+    NUMBN* temp_a = a;
+    NUMBN* temp_b = b;
+
+    while (NZER_N_B(temp_a) && NZER_N_B(temp_b)) {
+        if (COM_NN_D(temp_a, temp_b) == 2) {
+            temp_a = MOD_NN_N(temp_a, temp_b);
+        } else {
+            temp_b = MOD_NN_N(temp_b, temp_a);
+        }
+    }
+
+    return NZER_N_B(temp_a) ? temp_a : temp_b;
+}
---
+++ src/N/NZER_N_B.h
@@ -0,0 +1,21 @@
+#ifndef NZER_N_B_H
+#define NZER_N_B_H
+
+#include <stdbool.h>
+#include "numbN.h"
+
+/*
+Автор: Еремченко Пётр
+Группа: 5388
+*/
+
+/*
+Проверка на ноль:
+
+Принимает натуральное число
+Возвращает true если не равно нулю, иначе false
+
+*/
+bool NZER_N_B(NUMBN* a);
+
+#endif
